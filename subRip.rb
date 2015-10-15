2eb338ba9b89cedac8df3a6ea076120eaa16adcd
alejandromartincruz <socorreitor@gmail.com>
2015-10-15 19:14:05 +0200

    Aimed at times

--- subRip.rb
+++ subRip.rb
@@ -3,16 +3,21 @@ require 'pry'
 class ReadFile
 
 	def initialize
+		@lines = []
+		@times = []
+	end
+
+	def actions
+		readFile
+		search_time(@lines)
+		splitTime
+		puts @lines
 	end
 
 	def readFile
 		file=File.open("./GameOfThrones.srt", "r")
-		lines = file.readlines
-		#binding.pry
+		@lines = file.readlines
 		file.close
-
-		search_time(lines)
-		#times = lines.strptime(format='%H%M%S,%L')
 	end
 
 	def search_time(lines)
@@ -23,16 +28,27 @@ class ReadFile
 
 	def scan_line(line, index)
 		if line.include?("-->")
-			puts "Line with sign at index #{index}"
-			#do something
-		else
-			puts "no"
-			#do something else
+			time_position(index)
+		end
+
+	end
+
+	def time_position(pos)
+		@times << pos
+	end
+
+	def splitTime
+		@times.each do |index|
+			@lines[index] = @lines[index].split(" ")
 		end
 	end
+
+end
+
+class AddingTimes
 end
 
 class WriteFile
 end
 
-read = ReadFile.new.readFile
+read = ReadFile.new.actions
